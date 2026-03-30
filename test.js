28365d054c532d40514e78f92cef36f88dcc2a82
Marc <marcgomeztejedor@gmail.com>
2026-03-30 14:06:11 +0200

    created separate file for pow function, implemented edge cases for NaN output and added those test cases to the test.js file

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 describe("pow", function() {
 
   describe("raises x to power 3", function() {
+    
     function makeTestPower3(x) {
       let expected = x * x * x;
       it(`${x} in the power 3 is ${expected}`, function() {
@@ -13,7 +14,13 @@ describe("pow", function() {
     }
 
   });
-  
-  // more tests to follow here
+
+  it("for negative n the result is NaN", function() {
+    assert.isNaN(pow(2, -1));
+  });
+
+  it("for non-integer n the result is NaN", function() {
+    assert.isNaN(pow(2, 1.5));
+  });
 
 });
